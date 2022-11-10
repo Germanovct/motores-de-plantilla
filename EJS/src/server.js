21a770f94c9124d60c7f6441d694c3090d3041db
germano <germanovct@gmail.com>
2022-11-09 22:14:47 -0300

    motores de plantillas

---
+++ EJS/src/server.js
@@ -0,0 +1,21 @@
+
+
+const express = require ('express')
+
+const app = express ();
+ 
+
+app.set('views' , './views');
+
+app.set('view engine','ejs');
+
+app.get ('/datos', (req, res) => {
+    res.render('nivel' , req.query);
+});
+
+const port = 8080;
+
+const server = app.listen(port, () => {
+console.log(`Servidor http escuchendo en el puerto ${server.address().port}`);
+})
+server.on('error' , error => console.log(`Error en servidor ${error}`) );
